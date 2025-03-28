b4dd29e89f2805633e698dd59a59f335a184a235
IvanaFD <ivanafigueroa0936@gmail.com>
2025-03-27 18:21:09 -0600

    BST class

---
+++ BST.java
@@ -0,0 +1,28 @@
+import java.util.Comparator;
+
+public class BST<K,V> {
+    private Node<K,V> root;
+    private Comparator<K> comparator;
+
+    public BST(Comparator<K> comp){
+        root = null;
+        comparator = comp;
+    }
+    
+    public void insert (K k, V v) {
+        if (root == null) root = new Node<K, V> (k, v, null, null);
+        else root.insert(k, v, comparator);
+    }
+
+    public void inorder () {
+        if (root == null) System.out.println("Empty tree!");
+        else root.inorder();
+    }
+    
+    public V SearchforKey (K key) {
+        if (root == null) return null;
+        return root.SearchforKey(key, comparator);
+    }
+       
+
+}
--- Node.java
+++ Node.java
@@ -1,23 +1,59 @@
-public class Node<T extends Comparable<T>> {
-    T val;
-    Node<T> left;
-    Node<T> right;
+import java.util.Comparator;
 
-    public Node(T val) {
-        this.val = val;
+public class Node<K,V> {
+    private KeyValue<K,V> pair;
+    private Node<K,V> left;
+    private Node<K,V> right;
+
+    public Node(K k, V v, Node<K,V> left, Node<K,V> right) {
+        this.pair = new KeyValue<K,V>(k, v);
         this.left = null;
         this.right = null;
     }
 
     @Override
     public String toString(){
-        return "Valor" + val;
+        return "Valor" + pair;
 
     }
-        
+    
+    public void insert(K key, V value, Comparator<K> comp)  {
+        if (comp.compare(key, pair.getKey()) == 0) {
+            pair.setValue(value); 
+        } 
+        else if (comp.compare(key, pair.getKey()) < 0) {
+            if (left == null) {
+                left = new Node<>(key, value, null, null);
+            } else {
+                left.insert(key, value, comp);
+            }
+        } 
+        else {
+            if (right == null) {
+                right = new Node<>(key, value, null, null);
+            } else {
+                right.insert(key, value, comp);
+            }
+        }
+    }
 
-   
+    public void inorder() {
+        if (left != null) left.inorder();
+        System.out.println(pair.getKey() + " " + pair.getValue());
+        if (right != null) right.inorder();
+    }
 
+    public V SearchforKey(K key, Comparator<K> comp) {
+        if (comp.compare(key, pair.getKey()) == 0) {
+            return pair.getValue();
+        } 
+        else if (comp.compare(key, pair.getKey()) < 0) {
+            return left != null ? left.SearchforKey(key, comp) : null;
+        } 
+        else {
+            return right != null ? right.SearchforKey(key, comp) : null;
+        }
+    }
    
     
 }
